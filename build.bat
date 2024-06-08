37bf5756f864a79f9d164f384e69be70e02c1537
Jeremy Gao <145064583+WenSimEHRP@users.noreply.github.com>
2024-06-07 23:34:56 -0700

    Change: reMOD -> weMOD

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
-gcc -E -P -x c -o jptracks-remod.yagl sprites\jptracks.pyagl
-move jptracks-remod.yagl sprites\jptracks-remod.yagl
-yagl -e jptracks-remod.yagl
-move jptracks-remod.yagl jptracks-remod.grf
+gcc -E -P -x c -o jptracks-wemod.yagl sprites\jptracks.pyagl
+move jptracks-wemod.yagl sprites\jptracks-wemod.yagl
+yagl -e jptracks-wemod.yagl
+move jptracks-wemod.yagl jptracks-wemod.grf
 echo done!
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-gcc -E -P -x c -o jptracks-remod.yagl sprites/jptracks.pyagl &&
-    mv jptracks-remod.yagl sprites/jptracks-remod.yagl &&
-    yagl -e jptracks-remod.yagl &&
-    mv jptracks-remod.yagl jptracks-remod.grf &&
+gcc -E -P -x c -o jptracks-wemod.yagl sprites/jptracks.pyagl &&
+    mv jptracks-wemod.yagl sprites/jptracks-wemod.yagl &&
+    yagl -e jptracks-wemod.yagl &&
+    mv jptracks-wemod.yagl jptracks-wemod.grf &&
     echo "done!"
