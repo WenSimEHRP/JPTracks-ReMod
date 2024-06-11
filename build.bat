4ddbfcdae7030b746ed6965d42a1ebd4c23a71ca
Jeremy Gao <145064583+WenSimEHRP@users.noreply.github.com>
2024-06-10 21:51:00 -0700

    Remove: cmd building script

--- build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-gcc -E -P -x c -o jptracks-wemod.yagl sprites\jptracks.pyagl
-move jptracks-wemod.yagl sprites\jptracks-wemod.yagl
-yagl -e jptracks-wemod.yagl
-move jptracks-wemod.yagl jptracks-wemod.grf
-echo done!
