e0a832778c063573fc2991bb0535978ec51c2973
Jeremy Gao <145064583+WenSimEHRP@users.noreply.github.com>
2024-06-10 21:51:31 -0700

    Codechange: allow comments to be passed in final file

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-gcc -E -P -C -x c -o sprites/jptracks-wemod.yagl sprites/jptracks.pyagl &&
+# about flags:
+# -E: preprocess only; do not compile, assemble or link
+# -P: do not generate #line directives
+# -C: preserve comments
+# -x c: treat the input file as C code
+# -o: output file
+gcc -E -P -C -x c sprites/jptracks.pyagl > sprites/jptracks-wemod.yagl &&
     yagl -e jptracks-wemod.yagl &&
     mv jptracks-wemod.yagl jptracks-wemod.grf &&
     echo "done!"
