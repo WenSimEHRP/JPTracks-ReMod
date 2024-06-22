5c138c9d99fe323e204359cf4126c07df351a250
Jeremy Gao <145064583+WenSimEHRP@users.noreply.github.com>
2024-06-22 10:28:52 -0700

    ...

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 # -C: preserve comments
 # -x c: treat the input file as C code
 # -o: output file
-gcc -E -C -x c sprites/jptracks-wemod.pyagl > sprites/jptracks-wemod.yagl &&
+gcc -E -x c sprites/jptracks-wemod.pyagl > sprites/jptracks-wemod.yagl &&
     #yagl -e jptracks-wemod.grf &&
     /d/Data/Documents/GitHub/yagl/build/Debug/yagl.exe -e jptracks-wemod.grf &&
     echo "done!"
